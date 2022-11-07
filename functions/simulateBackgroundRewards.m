4f1bdbf30d33467fad327527ca43a7f3a770ae1f
Annie Taylor <annie.taylor@ucsf.edu>
2022-11-07 13:18:50 -0800

    Commented

--- functions/simulateBackgroundRewards.m
+++ functions/simulateBackgroundRewards.m
@@ -1,29 +1,40 @@
 function [eventlog] = simulateBackgroundRewards(numrewards, rewITI, rewlabel, rewmag, truncation)
-%SIMULATEBACKGROUNDREWARDS
+%SIMULATEBACKGROUNDREWARDS outputs an eventlog with exclusively background
+%rewards. Inputs may be scalar or arrays depending on the number of unique
+%background rewards specified.
+    % Set upper limit on ITI (only used if truncation == 1)
     maxrewITI = 3*rewITI;
+    % Initialize empty eventlog
     eventlog = NaN(sum(numrewards),3);
-    
-    running_idx = 0;
+    % Keep track of reward indices
+    running_idx = 0; 
+    % If multiple unique rewards, loop instance for each type
     for irw = 1:length(rewlabel)
-        running_time = 0;
-        for i = 1:numrewards(irw)
+        % Start simulation time at 0
+        running_time = 0; 
+        % For each reward (irw) simulate 'numrewards' timesteps
+        for i = 1:numrewards(irw) 
             running_idx = running_idx + 1;
-            new_ts = exprnd(rewITI(irw));
-            if truncation==1
-                if new_ts > maxrewITI(irw)
+            % Generate exp. dist. timestep for current reward instance
+            new_ts = exprnd(rewITI(irw)); 
+            if truncation==1 % If capping ITI at maxITI
+                while new_ts > maxrewITI(irw)
+                    % Simulate a new ts if maxITI is exceeded
                     new_ts = exprnd(rewITI(irw));
                 end
             end
-            eventlog(running_idx, 1) = rewlabel(irw);
-            eventlog(running_idx, 2) = new_ts + running_time;
-            eventlog(running_idx, 3) = rewmag(irw);
-            running_time = running_time + new_ts;
+            % Update eventlog
+            eventlog(running_idx, 1) = rewlabel(irw); % Column 1 = label
+            eventlog(running_idx, 2) = new_ts + running_time; % " 2 = ts
+            eventlog(running_idx, 3) = rewmag(irw); % " 3 = rew. magnitude
+            running_time = running_time + new_ts; % Update running time
         end
     end
     
+    % Resort eventlog by timestamp
     eventlog = sortrows(eventlog,2);
     
-    % if one reward reaches to its numrewards, finish the session
+    % Once any reward reaches to numrewards, finish the session
     lasttrial = min(cellfun(@(x) find(eventlog(:,1)==x,1,'last'),num2cell(rewlabel)));
     eventlog(lasttrial+1:end,:) = [];
 end
